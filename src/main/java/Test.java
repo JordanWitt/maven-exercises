78d15d1a386e5e43c8e0d5926b3e8a3da4f3362a
JordanWitt <wittjordan94@gmail.com>
2022-12-01 15:48:50 -0600

    maven exercises

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,19 @@
 import org.apache.commons.lang3.StringUtils;
 
 public class Test {
+
     public static void main(String[] args) {
-//        StringUtils.abbreviate()
+        String jordan = "jordan";
+        StringUtils.isNumeric(jordan);
+        StringUtils.swapCase(jordan);
+        StringUtils.reverse(jordan);
+
+        System.out.println(StringUtils.isNumeric("Is Jordan a number?" + jordan));
+        //nothing numeric always false
+        System.out.println(StringUtils.swapCase("Is Jordan in swap case CAPS " + jordan));
+        //swap uppercase to lower case and lower to upper
+        System.out.println(StringUtils.reverse("Is Jordan backwards" + jordan));
+        //prints everything backwards
 
     }
 }
